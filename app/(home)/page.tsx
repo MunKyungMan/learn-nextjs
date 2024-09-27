9d2dceca2b7c44c484a05c83a34139cea403ba30
Door_km <156165073+MunKyungMan@users.noreply.github.com>
2024-09-27 10:22:55 +0900

    Add files via upload

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,19 @@
+import { resolve } from "path";
+
 export const metadata = {
   title: "Home",
 };
 
-export default function Tomato() {
-  return (
-    <div>
-      <h1>Hello, NextJs!</h1>
-    </div>
-  );
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
