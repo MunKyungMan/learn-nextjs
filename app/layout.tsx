ff8417be08a7a0cb76364b81aa9bfc3dde9f31e0
MunKyungMan <oyesman1@naver.com>
2024-09-27 15:44:56 +0900

    Changes to be committed: 	modified:   Components/movie-info.tsx 	modified:   Components/movie-videos.tsx 	new file:   Components/movie.tsx 	modified:   Components/navigation.tsx 	modified:   app/(home)/page.tsx 	modified:   app/(movies)/movies/[id]/error.tsx 	modified:   app/(movies)/movies/[id]/page.tsx 	deleted:    app/about-us/company/jobs/sales/layout.tsx 	deleted:    app/about-us/company/jobs/sales/page.tsx 	modified:   app/about-us/page.tsx 	modified:   app/layout.tsx 	modified:   app/not-found.tsx 	deleted:    app/page.tsx 	new file:   styles/global.css 	new file:   styles/home.module.css 	new file:   styles/movie.module.css 	new file:   styles/navigation.module.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
+import "../styles/global.css";
 import { Metadata } from "next";
 import Navigation from "../Components/navigation";
 
 export const metadata: Metadata = {
   title: {
     template: "%s | Next Movies",
-    default: "Loading...",
+    default: "Next Movies",
   },
-  description: "The best movies on the bes framework",
+  description: "The best movies on the best framework",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
